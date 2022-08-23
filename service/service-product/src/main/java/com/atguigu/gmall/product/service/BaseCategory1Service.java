e2136fa6840de169112f6435bb9314a23d23dd01
Awei <1021744820@qq.com>
2022-08-23 15:10:34 +0800

    Initial commit

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/BaseCategory1Service.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.product.service;
+
+
+import com.atguigu.gmall.model.product.BaseCategory1;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author Awei
+* @description 针对表【base_category1(一级分类表)】的数据库操作Service
+* @createDate 2022-08-22 21:40:24
+*/
+public interface BaseCategory1Service extends IService<BaseCategory1> {
+
+}
